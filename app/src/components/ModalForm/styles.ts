d5d78f30ed33fe98079d78eca6a086b635139038
monitoria <presencadamonitoria@gmail.com>
2023-08-09 12:23:44 -0300

    change old style

--- app/src/components/ModalForm/styles.ts
+++ app/src/components/ModalForm/styles.ts
@@ -1,15 +1,33 @@
 import { styled } from "styled-components";
 
 export const Container = styled.form`
+  
+  .titleContainer {
+    padding: 1.5rem;
+    background: linear-gradient(to right, #509ca7, #22717e);
+  }
+  
+  h1{
+    color: white;
+    font-size: 18px;
+    font-weight: 900;
+    text-transform: uppercase;
+    margin-bottom: 1rem;
+  }
 
+  h2{
+    color: white;
+    font-size: 14px;
+    text-transform: uppercase;
+  }
+  
   input{
     width: 100%;
     padding: 0 1.5rem;
-    margin-top: 1.5rem;
     height: 4rem;
     border-radius: 0.25rem;
 
-    border: 1px solid #d7d7d7;
+    border: none;
     background: #e7e9ee;
 
     font-size: 1rem;
@@ -19,17 +37,24 @@ export const Container = styled.form`
       margin-top: 1rem;
     }
   }
+
+  label{
+    font-size: 14px;
+    font-weight: 700;
+  }
+
   button[type="submit"] {
       color: #e7e9ee;
       width: 100%;
       padding: 0 1.5rem;
+      margin-top: 1.5rem;
       height: 4rem;
-      background: #DDA448;
+      background: #22717e;
       border-radius: 0.25rem;
       border:0;
-      font-size: 1rem;
-      margin-top: 1.5rem;
-      font-weight: bold;
+      text-transform: uppercase;
+      font-size: 1.1rem;
+      font-weight: 950;
       transition: filter 0.2;
 
       &:hover{
@@ -37,4 +62,12 @@ export const Container = styled.form`
       }
     }
 
+    .inputContainer{
+      margin-top: 1.5rem;
+    }
+
+    hr{
+      margin-top 100px;
+      background-color: black;
+    }
 `
