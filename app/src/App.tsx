d81ce7a6d9f5c676f9d7812c7ce6cdca30571472
monitoria <presencadamonitoria@gmail.com>
2023-08-09 15:32:23 -0300

    modularize the function that handle the schedule

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { ModalForm } from "./components/ModalForm";
 import { GlobalStyle } from "./styles/global";
-
+import { monitorsSchedule, monitorInCharge } from "./monitorSchedule"
 
 export function App() {
 
@@ -10,28 +10,8 @@ export function App() {
   // escolhido é usado e enviado ao modal na hora de abrir o component modal (linha 47)
   const [linkAPI, setLinkAPI] = useState('')
   
-  function monitorInCharge(){
-    const date = new Date().getDay()
-    if(date === 1){ //segunda
-        return ['Raquel']
-      }
-      if(date === 2){ //terça
-        return ['Pedro', 'Dante']
-      }
-      if(date === 3){ //quarta
-        return ['Raquel', 'Dante', 'Artur']
-      }
-      if(date === 4){ //quinta
-        return ['Artur', 'Pedro']
-      }
-      if(date === 5){ //sexta
-        return ['Exercícios']
-      }
-      else{
-        return ['nada']
-      }
-  }
-  const monitores = monitorInCharge()
+  
+  const monitores = monitorInCharge(monitorsSchedule)
 
   function handleSetLinkApi(monitor: string) {
     if (monitor === 'Paz') {
