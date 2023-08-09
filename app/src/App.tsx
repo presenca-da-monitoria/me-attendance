000707728a1b417c093d4a815f71c21a4436ea6d
monitoria <presencadamonitoria@gmail.com>
2023-08-09 12:25:16 -0300

    add function to show monitor available

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,15 +4,34 @@ import { ModalForm } from "./components/ModalForm";
 import { GlobalStyle } from "./styles/global";
 
 
-
 export function App() {
-
-  const monitores = ['Paz', 'Pedro Maneiro', 'Raquel']
-
 
   //Esse setLinkAPI vai buscar os dados lá no component Monitor e atualiza ele quando clica-se no botão, o valor 
   // escolhido é usado e enviado ao modal na hora de abrir o component modal (linha 47)
   const [linkAPI, setLinkAPI] = useState('')
+  
+  function monitorInCharge(){
+    const date = new Date().getDay()
+    if(date === 1){ //segunda
+        return ['Raquel']
+      }
+      if(date === 2){ //terça
+        return ['Pedro', 'Dante']
+      }
+      if(date === 3){ //quarta
+        return ['Raquel', 'Dante', 'Artur']
+      }
+      if(date === 4){ //quinta
+        return ['Artur', 'Pedro']
+      }
+      if(date === 5){ //sexta
+        return ['Exercícios']
+      }
+      else{
+        return ['nada']
+      }
+  }
+  const monitores = monitorInCharge()
 
   function handleSetLinkApi(monitor: string) {
     if (monitor === 'Paz') {
