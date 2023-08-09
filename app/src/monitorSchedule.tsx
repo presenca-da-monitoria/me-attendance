80757fff9bef4344b822257342f7929682ab1569
monitoria <presencadamonitoria@gmail.com>
2023-08-09 16:15:27 -0300

    update object to just 2 attributes

--- app/src/monitorSchedule.tsx
+++ app/src/monitorSchedule.tsx
@@ -1,34 +1,29 @@
 interface monitorAvailable {
   nome: string;
-  h1: string;
-  h2: string;
+  horario: string;
+
 }
 
 export const monitorsSchedule = [ //Escala de horário dos monitores
     {
         nome: 'Raquel',
-        h1: '2T12', 
-        h2: '4T12'
+        horario: '2T12,4T12'
     },
     {
         nome: 'Artur',
-        h1:'2T12', 
-        h2:'4T12'
+        horario:'2T12,4T12'
     },
     {
         nome: 'Pedro',
-        h1: '3T12',
-        h2:'5T56'
+        horario: '3T12,5T56'
     },
     {
         nome: 'Dante',
-        h1: '3T56',
-        h2:'4T34'
+        horario: '3T56,4T34'
     },
     {
         nome: 'Exercícios',
-        h1: '6T12',
-        h2: ''
+        horario: '6T12'
     }
 ]
 
@@ -40,19 +35,19 @@ export function monitorInCharge(schedule: scheduleInterface){ //Função que, co
 
     if(date !== 7 && date >= 1 && date <= 5){
       for(let i=0; i < schedule.length - 1; i++){
-        let class1 = schedule[i].h1;
-        let class2 = schedule[i].h2;
+        let class1 = schedule[i].horario.slice(0,4);
+        let class2 = schedule[i].horario.slice(-4);
 
-        if(Number(class1.slice(0, 1)) == date || Number(class2.slice(0, 1)) == date){
-          dailyMonitors.push(schedule[i].nome);
+        if(Number(class1.slice(0, 1)) === date || Number(class2.slice(0, 1)) === date){
+          dailyMonitors.push(schedule[i]);
         }
       }
     } 
     else if(date === 6) {
-      dailyMonitors.push(schedule[4].nome)
+      dailyMonitors.push(schedule[4])
     }
-    else if(date == 7 || date || 8){
-      dailyMonitors.push(schedule[4].nome)
+    else if(date === 7 || date || 8){
+      dailyMonitors.push({nome: 'ninguém', horario: 'nenhum'}) //ninguém
     }
     return dailyMonitors
   }
