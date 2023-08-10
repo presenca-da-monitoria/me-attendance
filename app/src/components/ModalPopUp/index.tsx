07997f9c7a2c4112ea14de5c6b54018a5fac0e87
Raoni Silva <raoni.silve@gmail.com>
2023-08-10 17:20:57 -0300

    <ModalPopUp/> button functions

--- app/src/components/ModalPopUp/index.tsx
+++ app/src/components/ModalPopUp/index.tsx
@@ -2,19 +2,39 @@ import Modal from "react-modal"
 import { Container } from "./styled";
 
 interface ModalPopUpProps{
+
+    returnToSelection: () => void;
+    closeModalPopUp: () => void;
+
     isOpen: boolean;
+    isSend: boolean;
+
 }
 
-export function ModalPopUp({isOpen}:ModalPopUpProps) {
+Modal.setAppElement('#root')
+
+export function ModalPopUp({returnToSelection, closeModalPopUp, isOpen, isSend}:ModalPopUpProps) {
+
+    var modalOutput;
+
+    if (isSend) {
+        modalOutput = <>
+            <h1>Presença cadastrada com sucesso!</h1>
+        </>
+    } else {
+        <h1>Presença inválida, tente novamente.</h1>
+    }
+
+
     return(
         <Modal
         isOpen={isOpen}
+        
         >
             <Container>
-            <h1>{}</h1>
-            <button>Retornar</button>
+            {modalOutput}
+            <button onClick={() => {returnToSelection();closeModalPopUp()}}>Retornar</button>
             </Container>
-
         </Modal>
     )
 }
