2b00eb06465d5d8fdc7a976f6cf705af947be7dd
monitoria <presencadamonitoria@gmail.com>
2023-08-10 11:36:33 -0300

    new file to modularize the const

---
+++ app/src/features/monitorsSchedule.tsx
@@ -0,0 +1,22 @@
+export const monitorsSchedule = [ //Escala de horário dos monitores
+    {
+        nome: 'Raquel',
+        horario: '2T12,4T12'
+    },
+    {
+        nome: 'Artur',
+        horario:'2T12,4T12'
+    },
+    {
+        nome: 'Pedro',
+        horario: '3T12,5T56'
+    },
+    {
+        nome: 'Dante',
+        horario: '3T56,4T34'
+    },
+    {
+        nome: 'Exercícios',
+        horario: '6T12'
+    }
+]
