b84bcf95262e67bad59b6c164c4d639769635ab3
Raoni Silva <raoni.silvestre@hotmail.com>
2023-10-01 20:35:52 -0300

    modal style

--- app/src/components/ModalPopUp/styled.ts
+++ app/src/components/ModalPopUp/styled.ts
@@ -1,3 +1,53 @@
-import { styled } from "styled-components";
+import styled from 'styled-components';
 
-export const Container = styled.div``
+export const SuccessContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  max-height: 256px;
+  max-width: 512px;
+  height: 100%;
+  border: 1px solid #ddd;
+  margin: auto;
+  position: absolute;
+  top: 0; left: 0; right: 0; bottom: 0; /* Isso centralizará o componente no meio da tela */
+  background-color: #fff;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  border-radius: 1rem;
+`;
+
+export const SuccessMessageText = styled.div`
+  margin-bottom: 20px;
+  font-size: 1.5rem;
+`;
+
+export const ReturnButton = styled.button`
+
+font-size: 1rem;
+color: #fff;
+background: #15717E;
+border: 0;
+padding: 0 1rem;
+border-radius: 0.25rem;
+height: 3rem;
+width: 8rem;
+transition: filter 0.2s;
+
+&:hover { 
+    filter: brightness(0.9);
+}`
+
+export const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);  // Isso vai criar um fundo preto com 50% de transparência
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;  // Garante que o overlay e a mensagem aparecerão acima de outros elementos
+`;
