2ac2ed0a43976a0ab969eb0272402b1115eeee60
Raoni Silva <raoni.silvestre@hotmail.com>
2023-10-01 21:01:17 -0300

    Updated Monitor's Schedule

--- app/src/features/monitorsSchedule.tsx
+++ app/src/features/monitorsSchedule.tsx
@@ -1,22 +1,30 @@
 export const monitorsSchedule = [ //Escala de horário dos monitores
     {
         nome: 'Raquel',
-        horario: '2T12,4T12'
+        horario: '4M56,5T56'
     },
     {
         nome: 'Artur',
-        horario:'2T12,4T12'
+        horario:'2M56,3T56'
     },
     {
         nome: 'Pedro',
-        horario: '3T12,5T56'
+        horario: '2T12,5M56'
     },
     {
         nome: 'Dante',
-        horario: '3T56,4T34'
+        horario: '3M56,6M34'
     },
     {
-        nome: 'Exercícios',
-        horario: '6T12'
+        nome: 'Paz',
+        horario: '4T12,6T12'
+    },
+    {
+        nome: 'Gabi',
+        horario: '3T12,4T56'
+    },
+    {
+        nome: 'Daniel',
+        horario: '5M34,6T34'
     }
 ]
